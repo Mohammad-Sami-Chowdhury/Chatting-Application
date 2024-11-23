bd683635d464582a38bf96a7c095b7cd93119200
Mohammad Sami Chowdhury <contact.mohammadsami@gmail.com>
2024-11-23 12:34:13 +0600

    Project Updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,35 +6,68 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [emailFocused, setEmailFocused] = useState(false);
   const [password, setPassword] = useState("");
-  const [passwordFocused, setPasswordFocused] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
 
-  // Email Validation
-  const validateEmail = (value) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(value);
+  // EMAIL VALIDATION
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailError("");
   };
 
-  const handleEmailChange = (e) => {
-    const value = e.target.value;
-    setEmail(value);
-    setEmailError(validateEmail(value) ? "" : "Please enter a valid email");
-  };
-
-  const handlePasswordChange = (e) => {
+  // PASSWORD VALIDATION
+  const handlePassword = (e) => {
     setPassword(e.target.value);
+    setPasswordError("");
+  };
+
+  const handleLogin = () => {
+    if (!email) {
+      setEmailError("Please Enter Your Mail");
+      return;
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    ) {
+      setEmailError("Please Enter A Valid Email");
+      return;
+    }
+    setEmailError("");
+
+    if (!password) {
+      setPasswordError("Please Set A Password");
+      return;
+    } else if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long.");
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setPasswordError("Password must include at least one uppercase letter.");
+      return;
+    } else if (!/[a-z]/.test(password)) {
+      setPasswordError("Password must include at least one lowercase letter.");
+      return;
+    } else if (!/\d/.test(password)) {
+      setPasswordError("Password must include at least one number.");
+      return;
+    } else if (!/[@$!%*?&]/.test(password)) {
+      setPasswordError(
+        "Password must include at least one special character (@, $, !, %, *, ?, &)."
+      );
+      return;
+    }
+    setPasswordError("");
   };
   return (
     <section>
-      <div className="flex items-center">
-        <div className="w-1/2 ml-[180px]">
-          <h1 className="text-[#03014C] text-[34px] font-bold font-open leading-[45px] pb-[30px]">
+      <div className="md:flex items-center md:m-0 m-4 md:mt-0 mt-5">
+        <div className="md:w-1/2 md:ml-[180px]">
+          <h1 className="text-[#03014C] md:text-left text-center md:text-[34px] text-[22px] font-bold font-open md:leading-[45px] pb-[30px]">
             Login to your account!
           </h1>
-          <button className="group flex hover:border-[#5F35F5] duration-500 py-[22px] pl-[30px] items-center gap-2 border-[1px] border-[#b3b3c9] rounded-[8px] w-[220px] mb-[35px]">
+          <button className="group flex mx-auto md:mx-0 hover:border-[#5F35F5] duration-500 py-[22px] pl-[30px] items-center gap-2 border-[1px] border-[#b3b3c9] rounded-[8px] w-[220px] mb-[35px]">
             <img
               className="group-hover:rotate-[360deg] duration-500"
               src={google}
@@ -44,9 +77,10 @@ const Login = () => {
               Login with Google
             </p>
           </button>
-          <div className="relative  mb-[35px]">
+          {/* Email Field */}
+          <div className="relative mb-[35px]">
             <label
-              className={`absolute left-8 px-1 text-sm transition-all ${
+              className={`absolute left-8 px-1 text-sm transition-all duration-200 ${
                 email || emailFocused
                   ? "-top-[10px] px-4 bg-white text-[#5F35F5]"
                   : "top-[30px] text-gray-500"
@@ -57,20 +91,18 @@ const Login = () => {
             <input
               type="email"
               value={email}
-              onChange={handleEmailChange}
+              onChange={handleEmail}
               onFocus={() => setEmailFocused(true)}
               onBlur={() => setEmailFocused(false)}
-              className={`border-b-[1px] w-[368px] pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]  ${
-                emailError ? "border-red-500" : "border-gray-400"
-              }`}
+              className="border border-gray-300 md:w-[368px] w-full pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]"
             />
-            {emailError && (
-              <p className="text-red-500 text-sm mt-1">{emailError}</p>
-            )}
+            <p className="absolute uppercase top-[100%] left-[2%] font-nuni font-bold text-base text-red-600">{emailError}</p>
           </div>
+          {/* Password Field */}
+
           <div className="relative mb-[50px]">
             <label
-              className={`absolute left-8 px-1 text-sm transition-all ${
+              className={`absolute left-8 px-1 text-sm transition-all duration-200 ${
                 password || passwordFocused
                   ? "-top-[10px] px-4 bg-white text-blue-900"
                   : "top-[30px] text-gray-500"
@@ -81,31 +113,38 @@ const Login = () => {
             <input
               type={showPassword ? "text" : "password"}
               value={password}
-              onChange={handlePasswordChange}
+              onChange={handlePassword}
               onFocus={() => setPasswordFocused(true)}
               onBlur={() => setPasswordFocused(false)}
-              className="border-b-[1px] border-gray-400 w-[368px] pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]"
+              className="border border-gray-300 md:w-[368px] w-full pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]"
             />
             <button
               type="button"
-              className="absolute inset-y-0 left-[320px] flex items-center px-3 text-[20px] text-gray-500"
+              className="absolute inset-y-0 md:left-[320px] md:right-0 right-[20px] flex items-center px-3 text-[20px] text-gray-500"
               onClick={() => setShowPassword(!showPassword)}
             >
               {showPassword ? <FaEyeSlash /> : <FaEye />}
             </button>
+            <p className="absolute uppercase top-[100%] left-[2%] font-nuni font-bold text-base text-red-600">{passwordError}</p>
           </div>
-          <button className="px-[105px] py-[20px] bg-[#5F35F5] text-white rounded-[8px] duration-500 text-[20px] font-nuni font-semibold hover:bg-purple-700 focus:outline-none ">
+          <button
+            onClick={handleLogin}
+            className="md:w-[368px] w-full py-[20px] bg-[#5F35F5] text-white rounded-[8px] duration-500 text-[20px] font-nuni font-semibold hover:bg-purple-700 focus:outline-none "
+          >
             Login to Continue
           </button>
 
-          <p className="text-left font-open text-[13px] text-[#13014c] mt-[35px]">
+          <p className="md:text-left text-center font-open text-[13px] text-[#13014c] mt-[35px]">
             Already have an account ?
-            <a href="#" className="text-[#EA6C00] hover:text-[#ea6d00b0] duration-500 pl-1 font-bold">
+            <a
+              href="#"
+              className="text-[#EA6C00] hover:text-[#ea6d00b0] duration-500 pl-1 font-bold"
+            >
               Sign Up
             </a>
           </p>
         </div>
-        <div className="w-1/2">
+        <div className="w-1/2 hidden md:block">
           <img
             className="w-full h-screen object-cover"
             src={login}
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -7,54 +7,90 @@ function RegisterForm() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [nameError, setNameError] = useState("");
   const [emailFocused, setEmailFocused] = useState(false);
   const [nameFocused, setNameFocused] = useState(false);
   const [passwordFocused, setPasswordFocused] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
-  // Email Validation
-  const validateEmail = (value) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(value);
+  // EMAIL VALIDATION
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailError("");
   };
 
-  // Name Validation
-  const validateName = (value) => {
-    const nameRegex = /^[A-Za-z\s]+$/;
-    return nameRegex.test(value);
+  // USERNAME VALIDATION
+  const handleName = (e) => {
+    setName(e.target.value);
+    setEmailError("");
   };
 
-  const handleEmailChange = (e) => {
-    const value = e.target.value;
-    setEmail(value);
-    setEmailError(validateEmail(value) ? "" : "Please enter a valid email");
-  };
-
-  const handleNameChange = (e) => {
-    const value = e.target.value;
-    setName(value);
-    setNameError(validateName(value) ? "" : "Name should contain only letters");
-  };
-
-  const handlePasswordChange = (e) => {
+  // PASSWORD VALIDATION
+  const handlePassword = (e) => {
     setPassword(e.target.value);
+    setPasswordError("");
+  };
+
+  const handleSignup = () => {
+    if (!email) {
+      setEmailError("Please Enter Your Mail");
+      return;
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    ) {
+      setEmailError("Please Enter A Valid Email");
+      return;
+    }
+    setEmailError("");
+
+    if (!name) {
+      setNameError("Please Enter Your Name");
+      return;
+    } else if (!/^[a-zA-Z\s]+$/.test(name)) {
+      setNameError("Please Enter Your Valid Name");
+      return;
+    }
+    setNameError("");
+
+    if (!password) {
+      setPasswordError("Please Set A Password");
+      return;
+    } else if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long.");
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setPasswordError("Password must include at least one uppercase letter.");
+      return;
+    } else if (!/[a-z]/.test(password)) {
+      setPasswordError("Password must include at least one lowercase letter.");
+      return;
+    } else if (!/\d/.test(password)) {
+      setPasswordError("Password must include at least one number.");
+      return;
+    } else if (!/[@$!%*?&]/.test(password)) {
+      setPasswordError(
+        "Password must include at least one special character (@, $, !, %, *, ?, &)."
+      );
+      return;
+    }
+    setPasswordError("");
   };
 
   return (
-    <div className="flex items-center">
-      <div className="w-1/2 pl-[190px]">
-        <h2 className="text-[34px] font-bold text-left font-nuni text-[#11175D] leading-[47px] pb-[13px]">
+    <div className="md:flex items-center md:m-0 m-4 md:mt-0 mt-5">
+      <div className="md:w-1/2 h-1/2 md:h-full md:pl-[190px]">
+        <h2 className="md:text-[34px] text-[22px] text-center font-bold md:text-left font-nuni text-[#11175D] md:leading-[47px] pb-[13px]">
           Get started with easily register
         </h2>
-        <p className="text-left text-gray-500 text-[20px] font-nuni leading-[28px] pb-[40px]">
+        <p className="md:text-left text-center text-gray-500 md:text-[20px] text-base font-nuni leading-[28px] pb-[40px]">
           Free register and you can enjoy it
         </p>
 
         {/* Email Field */}
-        <div className="relative  mb-[35px]">
+        <div className="relative mb-[35px] md:block flex justify-center">
           <label
-            className={`absolute left-8 px-1 text-sm transition-all ${
+            className={`absolute left-8 px-1 text-sm transition-all duration-200 ${
               email || emailFocused
                 ? "-top-[10px] px-4 bg-white text-[#5F35F5]"
                 : "top-[30px] text-gray-500"
@@ -65,22 +101,20 @@ function RegisterForm() {
           <input
             type="email"
             value={email}
-            onChange={handleEmailChange}
+            onChange={handleEmail}
             onFocus={() => setEmailFocused(true)}
             onBlur={() => setEmailFocused(false)}
-            className={`border w-[368px] pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5] ${
-              emailError ? "border-red-500" : "border-gray-300"
-            }`}
+            className="border border-gray-300 md:w-[368px] w-full pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]"
           />
-          {emailError && (
-            <p className="text-red-500 text-sm mt-1">{emailError}</p>
-          )}
+          <p className="absolute uppercase top-[100%] left-[2%] font-nuni font-bold text-base text-red-600">
+            {emailError}
+          </p>
         </div>
 
         {/* Name Field */}
-        <div className="relative mb-[35px] ">
+        <div className="relative mb-[35px] md:block flex justify-center">
           <label
-            className={`absolute left-8 px-1 text-sm transition-all ${
+            className={`absolute left-8 px-1 text-sm transition-all duration-200 ${
               name || nameFocused
                 ? "-top-[10px] px-4 bg-white text-[#5F35F5]"
                 : "top-[30px] text-gray-500"
@@ -91,22 +125,21 @@ function RegisterForm() {
           <input
             type="text"
             value={name}
-            onChange={handleNameChange}
+            onChange={handleName}
             onFocus={() => setNameFocused(true)}
             onBlur={() => setNameFocused(false)}
-            className={`border w-[368px] pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5] ${
-              nameError ? "border-red-500" : "border-gray-300"
-            }`}
+            className="border border-gray-300 md:w-[368px] w-full pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]"
           />
-          {nameError && (
-            <p className="text-red-500 text-sm mt-1">{nameError}</p>
-          )}
+          <p className="absolute uppercase top-[100%] left-[2%] font-nuni font-bold text-base text-red-600">
+            {nameError}
+          </p>
         </div>
 
         {/* Password Field */}
-        <div className="relative mb-[50px]">
+
+        <div className="relative mb-[50px] md:block flex justify-center">
           <label
-            className={`absolute left-8 px-1 text-sm transition-all ${
+            className={`absolute left-8 px-1 text-sm transition-all duration-200 ${
               password || passwordFocused
                 ? "-top-[10px] px-4 bg-white text-blue-900"
                 : "top-[30px] text-gray-500"
@@ -117,34 +150,43 @@ function RegisterForm() {
           <input
             type={showPassword ? "text" : "password"}
             value={password}
-            onChange={handlePasswordChange}
+            onChange={handlePassword}
             onFocus={() => setPasswordFocused(true)}
             onBlur={() => setPasswordFocused(false)}
-            className="border w-[368px] pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]"
+            className="border border-gray-300 md:w-[368px] w-full pl-[52px] py-[26px] rounded-[8px] focus:outline-[#5F35F5]"
           />
           <button
             type="button"
-            className="absolute inset-y-0 left-[320px] flex items-center px-3 text-[20px] text-gray-500"
+            className="absolute inset-y-0 md:left-[320px] md:right-0 right-[20px] flex items-center px-3 text-[20px] text-gray-500"
             onClick={() => setShowPassword(!showPassword)}
           >
             {showPassword ? <FaEyeSlash /> : <FaEye />}
           </button>
+          <p className="absolute uppercase top-[100%] left-[2%] font-nuni font-bold text-base text-red-600">
+            {passwordError}
+          </p>
         </div>
 
         {/* Submit Button */}
-        <button className="px-[145px] py-[20px] bg-[#5F35F5] text-white rounded-[86px] duration-500 text-[20px] font-nuni font-semibold hover:bg-purple-700 focus:outline-none ">
+        <button
+          onClick={handleSignup}
+          className=" md:w-[368px] w-full md:mx-0 py-[20px] bg-[#5F35F5] text-white md:rounded-[86px] rounded-lg duration-500 text-[20px] font-nuni font-semibold hover:bg-purple-700 focus:outline-none "
+        >
           Sign up
         </button>
 
         {/* Sign In Link */}
-        <p className="pl-16 font-open text-[13px] text-[#13014c] mt-[35px]">
+        <p className="md:pl-16 pl-0 md:text-left text-center font-open text-[13px] text-[#13014c] mt-[35px]">
           Already have an account ?
-          <a href="#" className="text-[#EA6C00] hover:text-[#ea6d00b0] duration-500 pl-1 font-bold">
+          <a
+            href="#"
+            className="text-[#EA6C00] hover:text-[#ea6d00b0] duration-500 pl-1 font-bold"
+          >
             Sign In
           </a>
         </p>
       </div>
-      <div className="w-1/2">
+      <div className="hidden md:flex w-1/2">
         <img
           className="w-full object-cover h-screen"
           src={registration}
